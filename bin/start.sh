7cfd01a488858baced9dcb2d752785e2ca1d10b6
Artem Nechunaev <nechunaev@tutu.ru>
2019-03-29 18:24:46 +0300

    Add message on Varnish start

--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 varnishd -s malloc,128M -a :$CACHE_PORT -f /usr/share/app/config/varnish.vcl -n /usr/share/app/varnish
+echo "==> Reverse cache service @ http://${HOST}:${CACHE_PORT}"
 /usr/bin/env node ./dist/server
